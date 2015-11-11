2e8883e80458c60f540c32b580b9bbb20ba054c0
Michał Kalbarczyk <fazibear@gmail.com>
2015-11-11 16:39:21 +0100

    fix ' bug

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var exec = require('child_process').exec;
 // Identity loader
 module.exports = function(source) {
   var callback = this.async();
-  exec("ex2js '" + source + "' -ex -r elixirscript", function (error, stdout, stderr) {
+  var cmd = 'ex2js "' + source.replace(/"/g, '\\"') + '" -ex -r elixirscript';
+  exec(cmd, function (error, stdout, stderr) {
     if(error || stderr){
       callback(error || stderr, null);
     }
