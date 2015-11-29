b2360f5491e0b8bb85c634516ef728dcbfbe4272
Michał Kalbarczyk <fazibear@gmail.com>
2015-11-29 14:22:08 +0100

    update to new elixirscript

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var exec = require('child_process').exec;
 
-// Identity loader
 module.exports = function(source) {
   var callback = this.async();
-  var cmd = 'ex2js "' + source.replace(/"/g, '\\"') + '" -ex -r elixirscript';
+  var cmd = 'elixirscript "' + source.replace(/"/g, '\\"') + '" -ex -stp elixirscript';
   exec(cmd, function (error, stdout, stderr) {
     if(error || stderr){
       callback(error || stderr, null);
