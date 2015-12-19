0424446d5a4d5bb41523ee79d20c4f86b7a9f9b5
Michał Kalbarczyk <fazibear@gmail.com>
2015-12-19 17:49:37 +0100

    fix backslash

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec;
 
 module.exports = function(source) {
   var callback = this.async();
-  var cmd = 'elixirscript "' + source.replace(/"/g, '\\"') + '" -ex -stp elixirscript';
+  var cmd = 'elixirscript "' + source.replace(/\\/g, '\\\\').replace(/"/g, '\\"') + '" -ex -stp elixirscript';
   exec(cmd, function (error, stdout, stderr) {
     if(error || stderr){
       callback(error || stderr, null);
